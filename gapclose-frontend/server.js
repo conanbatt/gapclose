7eb420ca3a5f55ec7d323c4526090016b472d96b
gabriel benmergui <gabriel@scribd.com>
2017-05-21 21:12:26 -0300

    server forwarding

---
+++ gapclose-frontend/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const next = require('next')
+const { join } = require('path')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+var proxy = require('express-http-proxy');
+
+var apiProxy = proxy('http://localhost:3003/', {
+  proxyReqPathResolver: function (req, res) {
+      return require('url').parse(req.baseUrl).path;
+  }
+});
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.use('/api/*', apiProxy)
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(3000, err => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
